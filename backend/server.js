ce858ec7d36e11e41194d705d091fa7702533b2b
SabarinathanE <sabarielangovan13@gmail.com>
2024-09-07 18:10:03 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const portfolioRoute = require("./routes/portfolioRoute");
 app.use(express.json());
 app.use(cors(
   {
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000/"],
     methods: ["POST", "GET"],
     credentials: true
   }
@@ -20,6 +20,13 @@ app.use("/api/portfolio", portfolioRoute);
 const port = process.env.PORT || 5000;
 const path = require("path");
 
+if(process.env.NODE_ENV === "production") 
+{
+    app.use(express.static(path.join(__dirname, "client/build")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "client/build/index.html"));
+    } );
+}
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
